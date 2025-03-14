37ba947bc2d14b180543fead6cdbcce2f3329442
Boland Unfug <boland.unfug@gmail.com>
2025-03-14 13:46:34 -0600

    upscaled images

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 for (let i = 0; i < 5; i ++){
     var img = document.createElement("img");
-    img.src = "Urinal.jpg"
+    img.src = "Urinal(1).jpg"
     var src = document.getElementById("quiz");
     src.appendChild(img);
 }
